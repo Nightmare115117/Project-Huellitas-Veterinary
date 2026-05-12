94f1e8b956474ba3b8f3e4e66f2f96c5015aa01b
Nightmare11512 <l23051096@saltillo.tecnm.mx>
2026-05-12 00:46:40 -0600

    feat: add Spring gateway

---
+++ devops-gateway/src/test/java/com/huellitas/huellitas_gateway/HuellitasGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huellitas.huellitas_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuellitasGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
